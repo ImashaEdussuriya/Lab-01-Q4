9c2f4f347d5c34d7ef92a5a516d59ce5097145a3
Imasha <Imasha@SILVERBUNNY>
2024-04-28 00:29:18 +0530

    Add project files.

---
+++ Lab 01 Q4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of terms for the Fibonacci series:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci series:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
